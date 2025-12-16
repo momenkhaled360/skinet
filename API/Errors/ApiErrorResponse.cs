db0d28a3613d87fd4edb608d5ae52f78f638b9af
momen khaled <momenkhaled360@gmail.com>
2025-12-16 08:36:11 +0200

    Add exception handling middleware, improve validation error responses, and configure CORS

---
+++ API/Errors/ApiErrorResponse.cs
@@ -0,0 +1,16 @@
+﻿namespace API.Errors
+{
+    public class ApiErrorResponse
+    {
+        public ApiErrorResponse(int statusCode, string message, string? detalis)
+        {
+            StatusCode = statusCode;
+            Message = message;
+            Detalis = detalis;
+        }
+
+        public int StatusCode {  get; set; }
+        public string Message { get; set; }
+        public string? Detalis { get; set; }
+    }
+}
---
+++ API/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,43 @@
+﻿using API.Errors;
+using System.Net;
+using System.Text.Json;
+
+namespace API.Middleware
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate next;
+        private readonly IHostEnvironment env;
+        public ExceptionMiddleware(IHostEnvironment env,RequestDelegate next)
+        {
+            this.next=next;
+            this.env=env;
+        }
+        public async Task InvokeAsync(HttpContext context) 
+        {
+            try
+            {
+                await next(context);
+            }
+            catch (Exception ex) 
+            {
+             await HandleExceptionAsync(context,ex,env);
+            }
+        }
+
+        private static Task HandleExceptionAsync(HttpContext context, Exception ex, IHostEnvironment env)
+        {
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+            var response = env.IsDevelopment()
+                       ? new ApiErrorResponse(context.Response.StatusCode, ex.Message, ex.StackTrace)
+                       : new ApiErrorResponse(context.Response.StatusCode, ex.Message, "Internal Server Error");
+            var options = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
+
+            var json = JsonSerializer.Serialize(response, options);
+
+            return context.Response.WriteAsync(json);
+        }
+    }
+}
